0bc2f1446bfed3ebf4a00aebf72a849e232ef864
Dola Popoola <ddpopoola97@gmail.com>
2021-03-24 22:03:48 +0100

    subbed

--- index.js
+++ index.js
@@ -1,19 +1,20 @@
-window.onscroll = function() {myFunction()};
+// window.onscroll = function() {myFunction()};
 
-var header = document.getElementById("fixed");
-var sticky = header.offsetTop;
+// var header = document.getElementById("fixed");
+// var sticky = header.offsetTop;
 
-function myFunction() {
-  if (window.pageYOffset > sticky) {
-    header.classList.add("sticky");
-  } else {
-    header.classList.remove("sticky");
-  }
-}
+// function myFunction() {
+//   if (window.pageYOffset > sticky) {
+//     header.classList.add("sticky");
+//   } else {
+//     header.classList.remove("sticky");
+//   }
+// }
 
-function show(id) {
-  document.getElementById(id).style.visibility = "visible";
-}
-function hide(id) {
-  document.getElementById(id).style.visibility = "hidden";
-}
+// function show(id) {
+//   document.getElementById(id).style.visibility = "visible";
+// }
+// function hide(id) {
+//   document.getElementById(id).style.visibility = "hidden";
+// }
+
